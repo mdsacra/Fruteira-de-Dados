61a8815f35f5ed149686f955059a126a1d978e1b
Matheus Sacramento <mdsacra@gmail.com>
2020-05-31 17:31:06 -0300

    Logo alterado

--- fruteira-de-dados/Scripts/index.js
+++ fruteira-de-dados/Scripts/index.js
@@ -1,4 +1,32 @@
+
 function showNav() {
     var nav = document.querySelector('.navegacao')
     nav.classList.toggle('showNav')
 }
+
+function show(element, elementParent) {
+    var elPar = document.querySelector(elementParent)
+    elPar.setAttribute('id', 'focusel')
+    elPar.removeAttribute('class')
+    elPar.setAttribute('class', 'col-md-5')
+    var elem = document.querySelector(element)
+    elem.style.display = 'block'
+}
+
+function hide(element) {
+    var elem = document.querySelector(element)
+    elem.style.display = 'none'
+}
+
+class ShowHide extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Olá, minha gente!!!</p>
+            </div>
+        )
+    }
+}
+
+const container = document.getElementsByClassName('.container')
+ReactDOM.render(<ShowHide/>, container)
